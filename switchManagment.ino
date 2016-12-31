16851c53e6060d2c3882e4ceb0de1f1ee0544996
valtersitz <valtersitz@enseirb-matmeca.fr>
2016-12-31 15:39:40 +0100

    Delete switchManagment.ino

--- switchManagment.ino
+++
@@ -1,6 +0,0 @@
-// Managment of the Switch position between programmation mode and localization mode. Reads the state and turns the onchip LED accordingly
-
-void switchManagment() {
-  switchCheck = digitalRead(switchPin);     //prog if 1, localisation else
-  digitalWrite(13, switchCheck);            //LED on arduino is turned on when programming
-}
